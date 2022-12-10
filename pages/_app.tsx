fe16e16d792d3ae6be0f72b5a1d9fba61bddef47
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-12-10 10:33:40 -0300

    inicio do css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../styles/app.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
